91e070f39100c24597af0240b5ad4d6c0e7c9447
MotivatedUser <carbfixer@gmail.com>
2023-04-16 08:38:08 -0700

    Adds file structure, Header, Material-UI and React-icons

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, IconButton } from '@mui/material';
+import { FaBars } from 'react-icons/fa';
+
+const Header = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <FaBars />
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Shopping List
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
